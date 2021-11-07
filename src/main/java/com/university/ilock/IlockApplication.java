53021d27c27a2f943df344d308b5c85f684813bc
Vlad Munteanu <paulvlad34@gmail.com>
2021-11-07 19:33:53 +0200

    project set up

---
+++ src/main/java/com/university/ilock/IlockApplication.java
@@ -0,0 +1,13 @@
+package com.university.ilock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IlockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/university/ilock/IlockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.ilock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
